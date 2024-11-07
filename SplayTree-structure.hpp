c3787d0d87bd77ffa2b662464abec841002efd97
Felipe Torralba <fel.torra@gmail.com>
2024-11-07 19:23:54 -0300

    feat: added search method

--- SplayTree-structure.hpp
+++ SplayTree-structure.hpp
@@ -1,7 +1,31 @@
 /* Splay Tree */
 struct SplayTree {
-    int root;
+    int r;
     SplayTree* izq;
     SplayTree* der;
+
+    bool search(int x) {
+        /* buscar x como en ABB */
+        if (x == r) {
+            splay(x);
+            return true;
+        }
+        else if (x < r){
+            if (izq == nullptr){
+                splay(r); // splay del que debería ser el parent de x
+                return false;
+            }
+            return izq->search(x);
+        }
+        else if (x > r) {
+            if (der == nullptr){
+                splay(r); // splay del que debería ser el parent de x
+                return false;
+            }
+        }
+        return der->search(x);
+    }
+
+    void splay(int x){}
 
 };
