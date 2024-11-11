e265bb4e6bdb19836216cf011296faceaad2c266
elegoose <fel.torra@gmail.com>
2024-11-11 01:19:51 -0300

    fix: deleted code that already was in experiments file

--- main.cpp
+++ main.cpp
@@ -1,32 +1,17 @@
-#include "experiments.hpp"
-#include <math.h>
 #include <chrono>
+
+#include "experiments.hpp"
 using namespace chrono;
 
-// Genera un vector con N elementos distintos
-vector<int> random_vector(int N) {
-    vector<int> A(N);
-    map<int,int> m;
-    for (int i = 0; i < N; i++) {
-        int x = 1 + rand() % 100; // [1, 100]
-        if(m.find(i) == m.end()) {
-            A[i] = x;
-            m[x] = 1;
-        } else i--;
-    }
-    return A;
-}
-
 int main() {
-    int b = true;
     ABB* abb = new ABB;
     SplayTree* st = new SplayTree;
 
-    for (double i = 0.1; i < 1.0; i+=0.1) {
+    for (double i = 0.1; i < 1.0; i += 0.1) {
         int N = pow(10, 6) * i;
         int M = 100 * N;
 
-        vector<int> A = random_vector(N);
+        vector<int> A = create_random_vector(N);
 
         auto start = high_resolution_clock::now();
         first_scenario(abb, st, A, M);
